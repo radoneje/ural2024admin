fb16a055e5b24f93bdc7e2c7dca430d998abfa55
radoneje <53825282+radoneje@users.noreply.github.com>
2023-10-19 23:30:24 +0600

    tttgrgr

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1486,7 +1486,7 @@ router.get('/resendLKmail/:userid', async function (req, res, next) {
     try {
         let ret = await req.knex("t_email_messages").insert({
             subj: "Финансовый конгресс 2023: доступ к личному кабинету",
-            text: "/var/www/ifcAdmin/views/emails/300_link_to_lk.pug",
+            text: "/var/www/ural2024admin/views/emails/300_link_to_lk.pug",
             files: [],
             userid: req.params.userid
         }, "*")
