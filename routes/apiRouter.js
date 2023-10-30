9d8d1e8e4287300f9a7f9e024672a040342e4384
radoneje <53825282+radoneje@users.noreply.github.com>
2023-10-30 09:09:45 +0300

    tttgrgr

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -937,76 +937,20 @@ router.post('/room', async function (req, res, next) {
     }
 });
 router.get('/sendSpamToUsers', async function (req, res, next) {
-    //let text = await fs.promises.readFile(__dirname + "/../views/emails/330_spamlist02.html",'utf8')
+    //
+    //
+    let text = await fs.promises.readFile(__dirname + "/../views/emails/330_spamlist01.html",'utf8')
    // let text = await fs.promises.readFile(__dirname + "/../views/emails/350_spamlist01.html", 'utf8')
 
-    let text = pug.renderFile(__dirname + "/../views/emails/welcome2024/mail2023.pug", {})
+    //let text = pug.renderFile(__dirname + "/../views/emails/welcome2024/mail2023.pug", {})
 
 
     let attachments = [
-
-        {
+       // {
             //filename: 'logo.png',
-            path: __dirname + "/../views/emails/welcome2024/images/header.png",
-            cid: 'header' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png', views\emails\welcome2024\images\aboutLogo.png
-            path: __dirname + "/../views/emails/welcome2024/images/aboutLogo.png",
-            cid: 'aboutLogo' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',F:\uralcyberfin\ural2024admin\views\emails\welcome2024\images\diagram.png
-            path: __dirname + "/../views/emails/welcome2024/images/diagram.png",
-            cid: 'diagram' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\footer.png
-            path: __dirname + "/../views/emails/welcome2024/images/footer.png",
-            cid: 'footer' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\inDogitLogo.png
-            path: __dirname + "/../views/emails/welcome2024/images/inDogitLogo.png",
-            cid: 'inDogitLogo' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\infBezLogo.png
-            path: __dirname + "/../views/emails/welcome2024/images/infBezLogo.png",
-            cid: 'infBezLogo' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\infoIco.png
-            path: __dirname + "/../views/emails/welcome2024/images/infoIco.png",
-            cid: 'infoIco' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\keyLogo.png
-            path: __dirname + "/../views/emails/welcome2024/images/keyLogo.png",
-            cid: 'keyLogo' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\photos.png
-            path: __dirname + "/../views/emails/welcome2024/images/photos.png",
-            cid: 'photos' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\plusLogo.png
-            path: __dirname + "/../views/emails/welcome2024/images/plusLogo.png",
-            cid: 'plusLogo' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\securityLogo.png
-            path: __dirname + "/../views/emails/welcome2024/images/securityLogo.png",
-            cid: 'securityLogo' //my mistake was putting "cid:logo@cid" here!
-        },
-        {
-            //filename: 'logo.png',views\emails\welcome2024\images\zazitaLogo.png
-            path: __dirname + "/../views/emails/welcome2024/images/zazitaLogo.png",
-            cid: 'zazitaLogo' //my mistake was putting "cid:logo@cid" here!
-        },
-
-
+      //      path: __dirname + "/../views/emails/welcome2024/images/header.png",
+       //    cid: 'header' //my mistake was putting "cid:logo@cid" here!
+       // }
     ];
     let users = (await req.knex("t_users").where({isspamlist: true}))
     //let users = (await req.knex("t_tmp"));//.where({id: 1}))
